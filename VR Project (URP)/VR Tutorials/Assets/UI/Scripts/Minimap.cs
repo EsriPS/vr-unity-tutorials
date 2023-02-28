f3e149815fa98f54ba70efe6dc23d00e22272743
Lillie Bahrami <79878192+lillie-bahrami@users.noreply.github.com>
2023-02-27 23:01:48 -0700

    CreateMinimap() functionality

--- VR Project (URP)/VR Tutorials/Assets/UI/Scripts/Minimap.cs
+++ VR Project (URP)/VR Tutorials/Assets/UI/Scripts/Minimap.cs
@@ -1,18 +1,101 @@
 using System.Collections;
 using System.Collections.Generic;
+using System;
 using UnityEngine;
+using Esri.ArcGISMapsSDK.Components;
+using Unity.Mathematics;
+using Esri.HPFramework;
+using Esri.GameEngine.Geometry;
 
 public class Minimap : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    /* origin coords for testing
+     * 
+     * -72.9108982
+     * 41.3260946
+     * 10
+     */
+
+    public ArcGISMapComponent map; //to get origin coordinates
+    /* Adjust for demo purposes */
+    public int scale = 1;
+
+    public List<double3> locations; //starts with locs from FS
+    private List<GameObject> markers; //starts empty
+
+    public GameObject markerPrefab;
+
     void Start()
     {
-        
+        locations.Add(new double3(-72.9108982, 41.3260946, 10));
+        locations.Add(new double3(-72.9115539701574, 41.3254874554075, 20));
+        CreateMinimap();
     }
 
-    // Update is called once per frame
-    void Update()
+    #region Math
+
+    Vector3 WorldToMinimap(double3 loc)
     {
-        
+        /*
+         * Translation matrix - treats the lat/lon/alt position of map origin as (0,0,0),
+         * will transform other locations into that local space
+         * double4x4 object necessary for the ArcGISPoint properties
+         */
+        double4x4 DMinimapMatrix = new double4x4
+            (1, 0, 0, (map.OriginPosition.X * 1000 * -1),
+            0, 1, 0, (map.OriginPosition.Z * -0.1 ),
+            0, 0, 1, (map.OriginPosition.Y * 1000 * -1),
+            0, 0, 0, 1);
+        Matrix4x4 MinimapMatrix = DMinimapMatrix.ToMatrix4x4();
+
+        double3 DViewpointPos = new double3(loc.x * 1000, loc.z * 0.1, loc.y * 1000);
+        //double3 DViewpointPos = loc;
+        //DViewpointPos *= scale;
+        Vector3 ViewpointPos = DViewpointPos.ToVector3();
+
+        /*
+         * Returns scaled position relative to map origin
+         */
+        return MinimapMatrix.MultiplyPoint(ViewpointPos);
+    }
+
+    #endregion
+
+    #region Minimap Methods
+    public void CreateMinimap()
+    {
+        for(int i = 0; i < locations.Count; i+= 1)
+        {
+            AddMarker(i, false);
+        }
+    }
+
+
+
+    public void AddMarker(int index, bool addedByUser)
+    {
+        double3 loc = locations[index];
+        Vector3 markerPos = WorldToMinimap(loc);
+        Debug.Log(markerPos);
+        GameObject newMarker = Instantiate(markerPrefab, this.transform);
+
+        newMarker.transform.position = markerPos;
+        newMarker.GetComponent<MinimapMarker>().addedByUser = addedByUser;
+        //markers.Add(newMarker);
+    }
+
+    public void RemoveMarker()
+    {
+
+    }
+
+    #endregion
+
+
+    //called from markers
+    public void OnSelectMarker(int index)
+    {
+        StateManager.Instance.SetPlayerLocation(Convert.ToSingle(locations[index].x), Convert.ToSingle(locations[index].y), Convert.ToSingle(locations[index].z));
     }
 }
--- VR Project (URP)/VR Tutorials/Assets/UI/Scripts/MinimapMarker.cs
+++ VR Project (URP)/VR Tutorials/Assets/UI/Scripts/MinimapMarker.cs
@@ -1,18 +1,67 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
+using TMPro;
+
+struct markerState
+{
+    public Color color;
+    public Vector3 scale;
+
+    public markerState(Color c, Vector3 s)
+    {
+		color = c;
+        scale = s;
+    }
+}
 
 public class MinimapMarker : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public TextMeshPro number;
+    public GameObject cube;
+	public bool addedByUser = false; //set by Minimap.cs
 
-    // Update is called once per frame
-    void Update()
+    public enum mode { idle, hover, active };
+
+    Dictionary<mode, markerState> markerStates = new Dictionary<mode, markerState>()
     {
-        
-    }
+        { mode.idle, new markerState(new Color(1,1,1), new Vector3(0,0,0)) },
+        { mode.hover, new markerState(new Color(0.349f, 0.839f, 1), new Vector3(-0.025f, -0.025f, -0.025f)) },
+        { mode.active, new markerState(new Color(0,0.602f,0.945f), new Vector3(-0.035f, -0.035f, -0.035f)) }
+    };
+
+	public void UpdateMarker(mode initialMode, mode targetMode)
+	{
+		//frontPlateImg.color = Color.Lerp(buttonStates[initialMode].bgColor, buttonStates[targetMode].bgColor, 1f);
+		//icon.color = Color.Lerp(buttonStates[initialMode].fgColor, buttonStates[targetMode].fgColor, 1f);
+		//txt.color = Color.Lerp(buttonStates[initialMode].fgColor, buttonStates[targetMode].fgColor, 1f);
+		//fpTransform.anchoredPosition3D = basePos + buttonStates[targetMode].offset;
+	}
+
+	public void hoverEnter()
+	{
+		UpdateMarker(mode.idle, mode.hover);
+	}
+
+	public void hoverExit()
+	{
+		UpdateMarker(mode.hover, mode.idle);
+	}
+
+	public void selectEnter()
+	{
+		UpdateMarker(mode.hover, mode.active);
+	}
+
+	public void selectExit()
+	{
+		UpdateMarker(mode.active, mode.idle);
+	}
+
+	public void SetText(string newText)
+	{
+		number.text = newText;
+	}
 }
