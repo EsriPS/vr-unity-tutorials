6278ccba9c2d65b57dab5790fde21b9476b8f426
Lillie Bahrami <79878192+lillie-bahrami@users.noreply.github.com>
2023-02-28 17:07:01 -0700

    Place points correctly

--- VR Project (URP)/VR Tutorials/Assets/UI/Scripts/Minimap.cs
+++ VR Project (URP)/VR Tutorials/Assets/UI/Scripts/Minimap.cs
@@ -19,7 +19,7 @@ public class Minimap : MonoBehaviour
 
     public ArcGISMapComponent map; //to get origin coordinates
     /* Adjust for demo purposes */
-    public int scale = 1;
+    public int scale = 500;
 
     public List<double3> locations; //starts with locs from FS
     [SerializeField] List<GameObject> markers = new List<GameObject>(); //starts empty
@@ -29,7 +29,9 @@ public class Minimap : MonoBehaviour
     void Start()
     {
         locations.Add(new double3(-72.9108982, 41.3260946, 10));
-        locations.Add(new double3(-72.9115539701574, 41.3254874554075, 20));
+        locations.Add(new double3(-72.9124226410097, 41.3254348359225, 10));
+        locations.Add(new double3(-72.9091276205202, 41.3259003143511, 20));
+
         CreateMinimap();
     }
 
@@ -43,13 +45,13 @@ public class Minimap : MonoBehaviour
          * double4x4 object necessary for the ArcGISPoint properties
          */
         double4x4 DMinimapMatrix = new double4x4
-            (1, 0, 0, (map.OriginPosition.X * 1000 * -1),
+            (1, 0, 0, (map.OriginPosition.Y * scale * -1),
             0, 1, 0, (map.OriginPosition.Z * -0.1 ),
-            0, 0, 1, (map.OriginPosition.Y * 1000 * -1),
+            0, 0, 1, (map.OriginPosition.X * scale * -1),
             0, 0, 0, 1);
         Matrix4x4 MinimapMatrix = DMinimapMatrix.ToMatrix4x4();
 
-        double3 DViewpointPos = new double3(loc.x * 1000, loc.z * 0.1, loc.y * 1000);
+        double3 DViewpointPos = new double3(loc.y * scale, loc.z * 0.1, loc.x * scale);
         //double3 DViewpointPos = loc;
         //DViewpointPos *= scale;
         Vector3 ViewpointPos = DViewpointPos.ToVector3();
@@ -57,7 +59,12 @@ public class Minimap : MonoBehaviour
         /*
          * Returns scaled position relative to map origin
          */
-        return MinimapMatrix.MultiplyPoint(ViewpointPos);
+        Vector3 point = MinimapMatrix.MultiplyPoint3x4(ViewpointPos);
+
+        Matrix4x4 reflectionMatrix = new Matrix4x4();
+        reflectionMatrix = Matrix4x4.identity;
+        reflectionMatrix.SetColumn(0, new Vector4(-1, 0, 0, 0));
+        return reflectionMatrix.MultiplyPoint3x4(point);
     }
 
     #endregion
@@ -81,6 +88,7 @@ public class Minimap : MonoBehaviour
         GameObject newMarker = Instantiate(markerPrefab, this.transform);
 
         newMarker.transform.localPosition = markerPos;
+        newMarker.transform.Rotate(new Vector3(0, 1, 0), 180);
         newMarker.GetComponent<MinimapMarker>().addedByUser = addedByUser;
         newMarker.GetComponent<MinimapMarker>().locIndex = index;
 
--- VR Project (URP)/VR Tutorials/Assets/UI/Scripts/MinimapMarker.cs
+++ VR Project (URP)/VR Tutorials/Assets/UI/Scripts/MinimapMarker.cs
@@ -30,7 +30,7 @@ public class MinimapMarker : MonoBehaviour
     Dictionary<mode, markerState> markerStates = new Dictionary<mode, markerState>()
     {
         { mode.idle, new markerState(new Color(1,1,1), new Vector3(1,1,1)) },
-        { mode.hover, new markerState(new Color(0.257f, 0.812f, 0.945f), new Vector3(1.1f, 1.1f, 1.1f)) },
+        { mode.hover, new markerState(new Color(0f, 0.23f, 0.945f), new Vector3(1.1f, 1.1f, 1.1f)) },
         { mode.active, new markerState(new Color(0,0.825f,0.887f), new Vector3(1.3f, 1.3f, 1.3f)) }
     };
 
