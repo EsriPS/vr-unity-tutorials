09133cb1ac7e3c5686ab0039e7a26c15fdd9b0fd
Jeffrey Scarmazzi <jeffreyscarmazzi@gmail.com>
2023-04-24 15:46:10 -0400

    BSL Basics

--- VR Project (URP)/VR Tutorials/Assets/Scripts/BSL.cs
+++ VR Project (URP)/VR Tutorials/Assets/Scripts/BSL.cs
@@ -3,18 +3,20 @@ using Esri.GameEngine.Layers;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
+using Esri.Unity;
 
 public class BSL : MonoBehaviour
 {
-    [SerializeField] private ArcGISMapComponent _agsMap;
-
     void Start()
     {
-        var totalLayers = _agsMap.Layers.Count;
+        var agsGO  = GameObject.Find("ArcGISMap");
+        var agsMap = agsGO.GetComponent<ArcGISMapComponent>();
 
-        //for (var i = 0; i < totalLayers; i++)
-        //{
-        //    var layer = _agsMap.View.Map.Layers[i];
-        //}
+        var bsl = (ArcGISBuildingSceneLayer)agsMap.View.Map.Layers.At(0);
+
+        for (ulong i = 0; i < bsl.Sublayers.GetSize(); i++) {
+            bsl.Sublayers.At(i).IsVisible = true;
+        }                                 
     }
 }
